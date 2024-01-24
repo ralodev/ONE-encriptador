facac70c76b3078ca7ab6d235ef8e81497e72469
Raúl <ralo.dev@pm.me>
2024-01-24 14:24:23 -0600

    Update 24/01 - 1
    • Added more comments to the JS and HTML files
    • Added a new button & function: Delete
    • Updated the icon of the switch button
    • Changed the switch function to only move the answer text and not the input
    • Added new validations to disallow html injection
    • Added alerts for new cases
    • Updated the visuals of some elements

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const $input_text = document.getElementById("input");
 const $answer_panel = document.getElementById("answer_panel");
 const $error_panel = document.getElementById("error_panel");
 const $answer_text = document.getElementById("answer_text");
-const $error_text = document.getElementById("errot_text");
 const $button_switch = document.getElementById("switch_button");
+const $delete_button = document.getElementById("delete_button");
 // Diccionario de palabras
 const dictionary = {
   "e": "enter",
@@ -48,7 +48,7 @@ function encrypt(text) {
   text = text.toLowerCase();
   // Convertir a un array de caracteres
   var characters = text.split("");
-  // Mapear cada letra según el diccionario (si no está en el diccionario, se deja igual)
+  // Mapear cada letra según el diccionario (si no está en el diccionario, se deja igual) *(se puede cambiar por un bucle for)
   var encryptedText = characters.map(function (letter) {
     return dictionary[letter] || letter;
   });
@@ -70,16 +70,6 @@ function decrypt(text) {
   return decryptedText;
 }
 
-function showError(text) {
-  // Escribir el texto de error y mostrar el elemento
-  $error_text.innerHTML = text;
-  $error_text.style.display = "block";
-}
-function hideError() {
-  // Ocultar el elemento de error
-  $error_text.style.display = "none";
-}
-
 /*
 * Sección de eventos:
 * Los eventos se declaran con la función addEventListener, que recibe como
@@ -90,57 +80,101 @@ function hideError() {
 $button_encrypt.addEventListener("click", function () {
   // Si el campo de texto está vacío: mostrar error y ocultar el panel de respuesta
   if ($input_text.value == "") {
-    showError("No ingresaste ningún texto");
     showAlert("Debes ingresar texto", "error");
     show(false);
-  } else {
-    // Si no está vacío: ocultar el panel de error, encriptar el texto y mostrar el panel de respuesta
-    hideError();
-    $answer_text.innerHTML = encrypt($input_text.value);
-    show(true);
-    showAlert("Se ha encriptado el texto", "success");
+    return;
   }
+  let encryptedText = encrypt($input_text.value);
+  //Si el texto encriptado es el mismo que ya se ha encriptado, mostrar advertencia
+  if ($answer_text.innerHTML == encryptedText) {
+    showAlert("El resultado es el mismo", "warning");
+    return;
+  }
+  // Si no está vacío: ocultar el panel de error, encriptar el texto y mostrar el panel de respuesta
+  $answer_text.innerHTML = encryptedText
+  show(true);
+  showAlert("Se ha encriptado el texto", "success");
 });
+
 $button_decrypt.addEventListener("click", function () {
   // Si el campo de texto está vacío: mostrar error y ocultar el panel de respuesta
   if ($input_text.value == "") {
     show(false);
-    showError("No ingresaste ningún texto");
     showAlert("Debes ingresar texto", "error");
-  } else {
-    // Si no está vacío: ocultar el panel de error, desencriptar el texto y mostrar el panel de respuesta
-    hideError();
-    $answer_text.innerHTML = decrypt($input_text.value);
-    show(true);
-    showAlert("Se ha desencriptado el texto", "success");
+    return;
   }
+  let decryptedText = decrypt($input_text.value);
+  //Si el texto desencriptado es el mismo que ya se ha desencriptado, mostrar advertencia
+  if ($answer_text.innerHTML == decryptedText) {
+    showAlert("El resultado es el mismo", "warning");
+    return;
+  }
+  // Si no está vacío: ocultar el panel de error, desencriptar el texto y mostrar el panel de respuesta
+  $answer_text.innerHTML = decryptedText
+  show(true);
+  showAlert("Se ha desencriptado el texto", "success");
 });
+
 $button_copy.addEventListener("click", function () {
+  // Si el panel de respuesta está vacío, mostrar error
+  if ($answer_text.innerHTML.trim() == "") {
+    showAlert("No hay texto para copiar", "error");
+    return;
+  }
   // Copiar el texto de la respuesta al portapapeles
   navigator.clipboard.writeText($answer_text.innerHTML);
   showAlert("¡Texto copiado!", "success");
 });
+
 $button_switch.addEventListener("click", function () {
-  var backup = $input_text.value;
-  // Intercambiar el texto de la respuesta con el del campo de texto
+  if ($answer_text.innerHTML == "") {
+    // Si el panel de respuesta está vacío, mostrar error
+    showAlert("No hay texto para mover", "error");
+    return;
+  }
+  // Enviamos el texto de la respuesta al campo de texto
   $input_text.value = $answer_text.innerHTML;
-  $answer_text.innerHTML = backup.toLowerCase();
+  $answer_text.innerHTML = "";
+  showAlert("El texto ha sido movido", "info");
 });
 
+$delete_button.addEventListener("click", function () {
+  // Limpiar el campo de texto
+  $input_text.value = "";
+  // Limpiar el panel de respuesta
+  $answer_text.innerHTML = "";
+  // Ocultar el panel de respuesta y el botón de cambio
+  show(false);
+  showAlert("Se ha borrado todo!", "warning");
+});
 
-//Alertas (Uso de SweetAlert2)
+$input_text.addEventListener("input", function () {
+  // Definir dentro de /[]/ los símbolos que no se permiten
+  let pattern = /[<>áéíóúÁÉÍÓÚãõÃÕäëïöüÄËÏÖÜ]/;
+  // Verificar si el texto del campo de texto coincide con el patrón
+  if (pattern.test($input_text.value)) {
+    // Si coincide, mostrar error y eliminar el símbolo
+    showAlert("No se permite ese caracter", "error");
+    $input_text.value = $input_text.value.replace(pattern, "");
+  }
+});
+
+//---------------- FIN DEL CÓDIGO PRINCIPAL ----------------
+
+// Alertas de SweetAlert2 (ver: https://sweetalert2.github.io/)
 const Toast = Swal.mixin({
   toast: true,
-  position: "bottom-end",
+  position: "top",
   customClass: {
     popup: "!bg-[#fafbff] hover:opacity-50",
     timerProgressBar: "!bg-[#0a3871]",
   },
   showConfirmButton: false,
-  timer: 1200,
+  timer: 1500,
   timerProgressBar: true,
 });
 
+// Función para mostrar alertas
 function showAlert(title, icon) {
   Toast.fire({
     icon: icon,
