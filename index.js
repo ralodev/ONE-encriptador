fa85d4f5710ba242cacdada96c5c46e0b65a4053
Raúl <ralo.dev@pm.me>
2024-01-23 20:43:16 -0600

    Readability update

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-//Variables
-$button_encrypt = document.getElementById("encrypt");
-$button_decrypt = document.getElementById("decrypt");
-$button_copy = document.getElementById("copy");
-$input_text = document.getElementById("input");
-$answer_panel = document.getElementById("answer");
-$error_panel = document.getElementById("error");
-$answer = document.getElementById("answer_text");
-$error_text = document.getElementById("errot_text");
-$switch_button = document.getElementById("switch_button");
+
+/*
+* Sección de variables:
+* Por convención, las variables que hacen referencia a elementos del DOM
+* empiezan con el símbolo $, esto es para diferenciarlas más fácilmente
+*/
+const $button_encrypt = document.getElementById("encrypt_button");
+const $button_decrypt = document.getElementById("decrypt_button");
+const $button_copy = document.getElementById("copy_button");
+const $input_text = document.getElementById("input");
+const $answer_panel = document.getElementById("answer_panel");
+const $error_panel = document.getElementById("error_panel");
+const $answer_text = document.getElementById("answer_text");
+const $error_text = document.getElementById("errot_text");
+const $button_switch = document.getElementById("switch_button");
 
 const dictionary = {
   "e": "enter",
@@ -18,20 +23,23 @@ const dictionary = {
 };
 
 
-
-//Functions
+/*
+* Sección de funciones:
+* Las funciones se declaran con la palabra reservada function, seguida del nombre
+* de la función y entre paréntesis los parámetros que recibe, si no recibe
+* parámetros, se dejan los paréntesis vacíos.
+*/
 function show(bool) {
   if (bool) {
     $answer_panel.style.display = "flex";
     $error_panel.style.display = "none";
-    $switch_button.style.display = "block";
+    $button_switch.style.display = "block";
   } else {
     $answer_panel.style.display = "none";
     $error_panel.style.display = "flex";
-    $switch_button.style.display = "none";
+    $button_switch.style.display = "none";
   }
 }
-
 function encrypt(text) {
   var text = text.toLowerCase();
   var text = text.split("");
@@ -45,7 +53,6 @@ function encrypt(text) {
   var text = text.join("");
   return text;
 }
-
 function decrypt(text) {
   var text = text.toLowerCase();
   var text = text.replace(/ufat/g, "u");
@@ -55,45 +62,29 @@ function decrypt(text) {
   var text = text.replace(/enter/g, "e");
   return text;
 }
-
-function isAlreadyEncrypted(text) {
-  var text = text.toLowerCase();
-  let sensibility = 3;
-  for (var key in dictionary) {
-    if (text.includes(dictionary[key])) {
-      sensibility--;
-    }
-  }
-  if (sensibility <= 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 function showError(text) {
   $error_text.innerHTML = text;
   $error_text.style.display = "block";
 }
-
 function hideError() {
   $error_text.style.display = "none";
 }
 
-//Events
+/*
+* Sección de eventos:
+* Los eventos se declaran con la función addEventListener, que recibe como
+* parámetros el nombre del evento y la función que se ejecutará cuando se
+* dispare el evento.
+*/
 $button_encrypt.addEventListener("click", function () {
   if ($input_text.value == "") {
     showError("No ingresaste ningún texto");
     show(false);
   } else {
-    // if (isAlreadyEncrypted($input_text.value)) {
-    //   showError("El texto ingresado ya está encriptado");
-    //   return;
-    // }
     hideError();
     var text = $input_text.value;
     var answer = encrypt(text);
-    $answer.innerHTML = answer;
+    $answer_text.innerHTML = answer;
     show(true);
   }
 });
@@ -102,23 +93,19 @@ $button_decrypt.addEventListener("click", function () {
     show(false);
     showError("No ingresaste ningún texto");
   } else {
-    // if (!isAlreadyEncrypted($input_text.value)) {
-    //   showError("El texto ingresado no está encriptado");
-    //   return;
-    // }
     hideError();
     var text = $input_text.value;
     var answer = decrypt(text);
-    $answer.innerHTML = answer;
+    $answer_text.innerHTML = answer;
     show(true);
   }
 });
 $button_copy.addEventListener("click", function () {
-  var text = $answer.innerHTML;
+  var text = $answer_text.innerHTML;
   navigator.clipboard.writeText(text);
 });
-$switch_button.addEventListener("click", function () {
+$button_switch.addEventListener("click", function () {
   var text = $input_text.value;
-  $input_text.value = $answer.innerHTML;
-  $answer.innerHTML = text.toLowerCase();
+  $input_text.value = $answer_text.innerHTML;
+  $answer_text.innerHTML = text.toLowerCase();
 });
